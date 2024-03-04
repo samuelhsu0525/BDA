210b12ac2e281df513d9796abbe06a372de7545a
samuelhsu0525 <113759901+samuelhsu0525@users.noreply.github.com>
2024-03-04 22:27:13 +0800

    Add files via upload

--- HW1/ProblemA/b10902044/download.sh
+++ HW1/ProblemA/b10902044/download.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-gdown https://drive.google.com/drive/folders/15lizw5dbUyXB_staPakc8EAza9Wf_no7?usp=sharing -O videos/ --folder
+mkdir videos
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CAmdRTq1c9k32xmnhXbV-oAQXbrP7IPZ' -O videos/Screen_Recording_20240304_203558_Google.mp4
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CUnu7twP9s75GOR_3f6g-1XDJp-Us56q' -O videos/Screen_Recording_20240304_204208_Google.mp4
